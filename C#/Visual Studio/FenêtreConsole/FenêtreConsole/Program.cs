23ac0b483efac6ef7cdda819ca62545ff0b2abfc
Sébastien <sebastien.fischer@hotmail.com>
2021-10-29 16:41:55 +0200

    C# courses

---
+++ C#/Visual Studio/FenêtreConsole/FenêtreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FenêtreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C#/Visual Studio/Moyenne/Moyenne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Moyenne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Moyenne
+
+            int UserNumber;
+            double MeanValue = 0;
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Entrez un nombre : (-1 pour quitter)");
+                UserNumber = Int32.Parse(Console.ReadLine());
+
+                if (UserNumber == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    MeanValue += UserNumber;
+                    i++;
+                }
+
+            } while (true);
+
+            Console.WriteLine("La moyenne : " + (MeanValue/i));
+
+            #endregion
+        }
+    }
+}
